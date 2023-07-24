7b03031ef9cf4e93577a10e584ffff69caed6dab
TharinduDR <rhtdranasinghe@gmail.com>
2023-07-24 22:05:03 +0100

    001: Initial commit

--- algo/transformer_model/evaluation.py
+++ algo/transformer_model/evaluation.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.stats import pearsonr, spearmanr
+from sklearn.metrics import mean_absolute_error
 
 
 def pearson_corr(preds, labels):
@@ -12,3 +13,17 @@ def spearman_corr(preds, labels):
 
 def rmse(preds, labels):
     return np.sqrt(((np.asarray(preds, dtype=np.float32) - np.asarray(labels, dtype=np.float32)) ** 2).mean())
+
+
+def print_stat(data_frame, real_column, prediction_column):
+    data_frame = data_frame.sort_values(real_column)
+
+    pearson = pearson_corr(data_frame[real_column].tolist(), data_frame[prediction_column].tolist())
+    spearman = spearman_corr(data_frame[real_column].tolist(), data_frame[prediction_column].tolist())
+    rmse_value = rmse(data_frame[real_column].tolist(), data_frame[prediction_column].tolist())
+    mae = mean_absolute_error(data_frame[real_column].tolist(), data_frame[prediction_column].tolist())
+
+    textstr = 'RMSE=%.4f\nMAE=%.4f\nPearson Correlation=%.4f\nSpearman Correlation=%.4f' % (
+        rmse_value, mae, pearson, spearman)
+
+    print(textstr)
--- experiments/run_transformer.py
+++ experiments/run_transformer.py
@@ -1,5 +1,14 @@
-import pandas as pd
+import os
+import shutil
 
+import pandas as pd
+import numpy as np
+import torch
+from sklearn.metrics import mean_absolute_error
+
+from transformer_model.evaluation import pearson_corr, spearman_corr, print_stat
+from transformer_model.model_args import LCPArgs
+from transformer_model.run_model import LCPModel
 
 train = pd.read_csv("data/v0.01_CompLex-pt_train.tsv", sep="\t")
 dev = pd.read_csv("data/v0.01_CompLex-pt_dev.tsv", sep="\t")
@@ -20,3 +29,37 @@ test = test.rename(columns={'pt_sentence': 'text_b', 'avg_complexity': 'labels'}
 train = train[["text_a", "text_b", "labels"]]
 dev = dev[["text_a", "text_b", "labels"]]
 test = test[["text_a", "text_b", "labels"]]
+
+test_sentence_pairs = list(map(list, zip(test['text_a'].to_list(), test['text_b'].to_list())))
+test_preds = np.zeros((len(test), 5))
+for i in range(5):
+
+    args = LCPArgs()
+    args.eval_batch_size = 16
+    args.evaluate_during_training = True
+    args.evaluate_during_training_steps = 120
+    args.evaluate_during_training_verbose = True
+    args.logging_steps = 120
+    args.learning_rate = 1e-5
+    args.manual_seed = 777*i
+    args.max_seq_length = 120
+    args.model_type = "bert"
+    args.model_name = "bert-base-multilingual-cased"
+    args.num_train_epochs = 5
+    args.save_steps = 120
+    args.train_batch_size = 8
+
+    if os.path.exists(args.output_dir) and os.path.isdir(
+            args.output_dir):
+        shutil.rmtree(args.output_dir)
+        model = LCPModel(args.model_type, args.model_name, num_labels=1, use_cuda=torch.cuda.is_available(),
+                                    args=args)
+        model.train_model(train, eval_df=dev, pearson_corr=pearson_corr, spearman_corr=spearman_corr,
+                          mae=mean_absolute_error)
+        model = LCPModel(args.model_type, args.best_model_dir, num_labels=1,
+                                    use_cuda=torch.cuda.is_available(), rgs=args)
+        predictions, raw_outputs = model.predict(test_sentence_pairs)
+        test_preds[:, i] = predictions
+
+test['predictions'] = test_preds.mean(axis=1)
+print_stat(test, 'labels', 'predictions')
