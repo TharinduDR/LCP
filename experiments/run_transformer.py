5eac89d47f32f6cb018d8bdae1cc4a529283af55
TharinduDR <rhtdranasinghe@gmail.com>
2023-07-24 21:21:34 +0100

    001: Initial commit

---
+++ experiments/run_transformer.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+train = pd.read_csv("data/v0.01_CompLex-pt_train.tsv", sep="\t")
+dev = pd.read_csv("data/v0.01_CompLex-pt_dev.tsv", sep="\t")
+test = pd.read_csv("data/v0.01_CompLex-pt_test.tsv", sep="\t")
+
