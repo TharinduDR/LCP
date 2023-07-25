5f8867cc312804b7c20f4deb7f60f25323d171ad
TharinduDR <rhtdranasinghe@gmail.com>
2023-07-25 17:18:59 +0100

    001: Initial commit

--- experiments/run_transformer.py
+++ experiments/run_transformer.py
@@ -18,13 +18,13 @@ train = train[["genre", "pt_sentence", "pt_word", "avg_complexity"]]
 dev = dev[["genre", "pt_sentence", "pt_word", "avg_complexity"]]
 test = test[["genre", "pt_sentence", "pt_word", "avg_complexity"]]
 
-# train["text_a"] = train["genre"] + ' ' + train["pt_word"]
-# dev["text_a"] = dev["genre"] + ' ' + dev["pt_word"]
-# test["text_a"] = test["genre"] + ' ' + test["pt_word"]
+train["text_a"] = train["genre"] + ' ' + train["pt_word"]
+dev["text_a"] = dev["genre"] + ' ' + dev["pt_word"]
+test["text_a"] = test["genre"] + ' ' + test["pt_word"]
 
-train["text_a"] = train["pt_word"]
-dev["text_a"] = dev["pt_word"]
-test["text_a"] = test["pt_word"]
+# train["text_a"] = train["pt_word"]
+# dev["text_a"] = dev["pt_word"]
+# test["text_a"] = test["pt_word"]
 
 train = train.rename(columns={'pt_sentence': 'text_b', 'avg_complexity': 'labels'}).dropna()
 dev = dev.rename(columns={'pt_sentence': 'text_b', 'avg_complexity': 'labels'}).dropna()
