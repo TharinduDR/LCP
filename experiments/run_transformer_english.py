63799f708f7a8963933652ff8f0e4625f9875060
TharinduDR <rhtdranasinghe@gmail.com>
2023-09-12 17:41:40 +0100

    001: Initial commit

--- experiments/run_transformer_english.py
+++ experiments/run_transformer_english.py
@@ -52,8 +52,8 @@ for i in range(5):
     model_args.learning_rate = 5e-6
     model_args.manual_seed = 777 * i
     model_args.max_seq_length = 256
-    model_args.model_type = "xlmroberta"
-    model_args.model_name = "xlm-roberta-large"
+    model_args.model_type = "bert"
+    model_args.model_name = "bert-large-cased"
     model_args.num_train_epochs = 5
     model_args.output_dir = "english_outputs/"
     model_args.save_steps = 300
