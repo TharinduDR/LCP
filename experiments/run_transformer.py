e12c32ff88c6af8503fffec07c658dc63cf65571
TharinduDR <rhtdranasinghe@gmail.com>
2023-09-12 15:53:09 +0100

    001: Initial commit

--- experiments/run_transformer.py
+++ experiments/run_transformer.py
@@ -43,7 +43,7 @@ train = train.sample(frac=1).reset_index(drop=True)
 
 for i in range(5):
     model_args = LCPArgs()
-    model_args.best_model_dir = "portuguese_outputs/best_model"
+    model_args.best_model_dir = ""
     model_args.eval_batch_size = 16
     model_args.evaluate_during_training = True
     model_args.evaluate_during_training_steps = 120
@@ -53,7 +53,7 @@ for i in range(5):
     model_args.manual_seed = 777 * i
     model_args.max_seq_length = 256
     model_args.model_type = "bert"
-    model_args.model_name = "neuralmind/bert-base-portuguese-cased"
+    model_args.model_name = "bert-base-multilingual-cased"
     model_args.num_train_epochs = 5
     model_args.output_dir = "portuguese_outputs/"
     model_args.save_steps = 120
