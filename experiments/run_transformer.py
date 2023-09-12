0e849ec3398860ca52f319690cd74ea15177444c
TharinduDR <rhtdranasinghe@gmail.com>
2023-09-12 15:01:45 +0100

    001: Initial commit

--- experiments/run_transformer.py
+++ experiments/run_transformer.py
@@ -53,7 +53,7 @@ for i in range(5):
     model_args.manual_seed = 777 * i
     model_args.max_seq_length = 256
     model_args.model_type = "bert"
-    model_args.model_name = "neuralmind/bert-large-portuguese-cased"
+    model_args.model_name = "neuralmind/bert-base-portuguese-cased"
     model_args.num_train_epochs = 5
     model_args.output_dir = "portuguese_outputs/"
     model_args.save_steps = 120
