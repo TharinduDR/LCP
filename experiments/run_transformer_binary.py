00b5b4c28aa04a22f16ac4a64f0b1e151d1d3bfb
TharinduDR <rhtdranasinghe@gmail.com>
2023-09-14 10:03:37 +0100

    001: Initial commit

--- experiments/run_transformer_binary.py
+++ experiments/run_transformer_binary.py
@@ -7,7 +7,7 @@ import torch
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
-from algo.transformer_model.evaluation import pearson_corr, spearman_corr, print_stat
+from algo.transformer_model.evaluation import pearson_corr, spearman_corr, print_stat, print_binary_stat
 from algo.transformer_model.model_args import LCPArgs
 from algo.transformer_model.run_model import LCPModel
 
@@ -30,9 +30,9 @@ dev['context2'] = dev.apply(lambda row: modify_sentence(row['word2'], row['conte
 test['context1'] = test.apply(lambda row: modify_sentence(row['word1'], row['context']), axis=1)
 test['context2'] = test.apply(lambda row: modify_sentence(row['word2'], row['context2']), axis=1)
 
-train = train[["context1", "context2", "binary_comparative_val"]]
-dev = dev[["context1", "context2", "binary_comparative_val"]]
-test = test[["context1", "context2", "binary_comparative_val"]]
+train = train[["genre", "context1", "context2", "binary_comparative_val"]]
+dev = dev[["genre", "context1", "context2", "binary_comparative_val"]]
+test = test[["genre", "context1", "context2", "binary_comparative_val"]]
 
 train = train.rename(columns={'context1': 'text_a', 'context2': 'text_b', 'binary_comparative_val': 'labels'}).dropna()
 dev = dev.rename(columns={'context1': 'text_a', 'context2': 'text_b', 'binary_comparative_val': 'labels'}).dropna()
@@ -43,7 +43,7 @@ test_preds = np.zeros((len(test), 5))
 
 for i in range(5):
     model_args = LCPArgs()
-    model_args.best_model_dir = "portuguese_outputs/best_model"
+    model_args.best_model_dir = "portuguese_binary_outputs/best_model"
     model_args.eval_batch_size = 16
     model_args.evaluate_during_training = True
     model_args.evaluate_during_training_steps = 120
@@ -55,9 +55,38 @@ for i in range(5):
     model_args.model_type = "xlmroberta"
     model_args.model_name = "xlm-roberta-large"
     model_args.num_train_epochs = 5
-    model_args.output_dir = "portuguese_outputs/"
+    model_args.output_dir = "portuguese_binary_outputs/"
     model_args.save_steps = 120
     model_args.train_batch_size = 8
-    model_args.wandb_project = "LCP"
-    model_args.regression = True
+    model_args.wandb_project = "LCP_BINARY"
+
+    if os.path.exists(model_args.output_dir) and os.path.isdir(
+            model_args.output_dir):
+        shutil.rmtree(model_args.output_dir)
+    model = LCPModel(model_args.model_type, model_args.model_name, use_cuda=torch.cuda.is_available(),
+                     args=model_args)
+    model.train_model(train, eval_df=dev, pearson_corr=pearson_corr, spearman_corr=spearman_corr,
+                      mae=mean_absolute_error)
+    model = LCPModel(model_args.model_type, model_args.best_model_dir, num_labels=1,
+                     use_cuda=torch.cuda.is_available(), args=model_args)
+    predictions, raw_outputs = model.predict(test_sentence_pairs)
+    test_preds[:, i] = predictions
+
+final_predictions = []
+for pred_row in test_preds:
+    all_predictions = pred_row.tolist()
+    final_predictions.append(int(max(set(all_predictions), key=all_predictions.count)))
+
+test['predictions'] = final_predictions
+
+print_binary_stat(test, 'labels', 'predictions')
+test.to_csv("test_predictions.csv", sep="\t", index=False)
+
+genres = test['genre'].unique()
+for genre in genres:
+    print(genre)
+    filtered_predictions = test.loc[test['genre'] == genre]
+    print_binary_stat(filtered_predictions, 'labels', 'predictions')
+    print("=================")
+
 
