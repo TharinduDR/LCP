ba96ea84b1513a2b74ea96a3323ea9082e2c83a9
TharinduDR <rhtdranasinghe@gmail.com>
2023-10-04 09:29:07 +0100

    001: Transfer learning experiments

--- experiments/run_transformer_english.py
+++ experiments/run_transformer_english.py
@@ -31,19 +31,19 @@ train, dev = train_test_split(train, test_size=0.2)
 test_sentence_pairs = list(map(list, zip(test['text_a'].to_list(), test['text_b'].to_list())))
 test_preds = np.zeros((len(test), 5))
 
-# portuguese_test = pd.read_csv("data/v0.01_CompLex-pt_test.tsv", sep="\t")
-# portuguese_test = portuguese_test[["genre", "pt_sentence", "pt_word", "avg_complexity"]]
-# portuguese_test["text_a"] = portuguese_test["genre"] + ' ' + portuguese_test["pt_word"]
-# portuguese_test = portuguese_test.rename(columns={'pt_sentence': 'text_b', 'avg_complexity': 'labels'}).dropna()
-#
-# portuguese_test = portuguese_test[["text_a", "text_b", "labels"]]
-#
-# portuguese_test_sentence_pairs = list(map(list, zip(portuguese_test['text_a'].to_list(), portuguese_test['text_b'].to_list())))
-# portuguese_test_preds = np.zeros((len(portuguese_test), 5))
+portuguese_test = pd.read_csv("data/v0.01_CompLex-pt_test.tsv", sep="\t")
+portuguese_test = portuguese_test[["genre", "pt_sentence", "pt_word", "avg_complexity"]]
+portuguese_test["text_a"] = portuguese_test["genre"] + ' ' + portuguese_test["pt_word"]
+portuguese_test = portuguese_test.rename(columns={'pt_sentence': 'text_b', 'avg_complexity': 'labels'}).dropna()
+
+portuguese_test = portuguese_test[["text_a", "text_b", "labels"]]
+
+portuguese_test_sentence_pairs = list(map(list, zip(portuguese_test['text_a'].to_list(), portuguese_test['text_b'].to_list())))
+portuguese_test_preds = np.zeros((len(portuguese_test), 5))
 
 for i in range(5):
     model_args = LCPArgs()
-    model_args.best_model_dir = "english_outputs/best_model"
+    model_args.best_model_dir = "english_outputs/mbert/best_model"
     model_args.eval_batch_size = 16
     model_args.evaluate_during_training = True
     model_args.evaluate_during_training_steps = 300
@@ -55,7 +55,7 @@ for i in range(5):
     model_args.model_type = "bert"
     model_args.model_name = "bert-base-multilingual-cased"
     model_args.num_train_epochs = 5
-    model_args.output_dir = "english_outputs/"
+    model_args.output_dir = "english_outputs/mbert/"
     model_args.save_steps = 300
     model_args.train_batch_size = 8
     model_args.wandb_project = "LCP"
@@ -73,8 +73,8 @@ for i in range(5):
                      use_cuda=torch.cuda.is_available(), args=model_args)
     predictions, raw_outputs = model.predict(test_sentence_pairs)
     test_preds[:, i] = predictions
-    # portuguese_predictions, portuguese_raw_outputs = model.predict(portuguese_test_sentence_pairs)
-    # portuguese_test_preds[:, i] = portuguese_predictions
+    portuguese_predictions, portuguese_raw_outputs = model.predict(portuguese_test_sentence_pairs)
+    portuguese_test_preds[:, i] = portuguese_predictions
 
 test['predictions'] = test_preds.mean(axis=1)
 print_stat(test, 'labels', 'predictions')
@@ -95,6 +95,17 @@ for genre in genres:
     print_stat(filtered_predictions, 'labels', 'predictions')
     print("=================")
 
-# portuguese_test['predictions'] = portuguese_test_preds.mean(axis=1)
-# print_stat(portuguese_test, 'labels', 'predictions')
-# portuguese_test.to_csv("portuguese_test_predictions.csv", sep="\t", index=False)
+print("Portuguese predictions")
+
+portuguese_test['predictions'] = portuguese_test_preds.mean(axis=1)
+print_stat(portuguese_test, 'labels', 'predictions')
+portuguese_test.to_csv("portuguese_test_predictions.csv", sep="\t", index=False)
+
+portuguese_test["genre"] = portuguese_test['text_a'].apply(genre_function)
+
+genres = portuguese_test['genre'].unique()
+for genre in genres:
+    print(genre)
+    filtered_predictions = portuguese_test.loc[portuguese_test['genre'] == genre]
+    print_stat(filtered_predictions, 'labels', 'predictions')
+    print("=================")
